950f2fd06e1f96798dae0e55f6c70c75736d8827
HeavySin <lifeisbug0@gmail.com>
2023-09-14 01:14:14 +0300

    function `endwithignorecase` implemented & added.

--- src/unstdstring.c
+++ src/unstdstring.c
@@ -4,3 +4,9 @@ bool endswith(const char *const stringArg, const char checkOnArg) {
     if (stringArg[strlen(stringArg) - 1] == checkOnArg) { return true;}
     else {return false;}
 }
+
+
+bool endswithignorecase(const char *const stringArg, const char checkOnArg) {
+    if (tolower(stringArg[strlen(stringArg) - 1]) == tolower(checkOnArg)) { return true;}
+    else {return false;}
+}
--- src/unstdstring.h
+++ src/unstdstring.h
@@ -1,4 +1,6 @@
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 bool endswith(const char *const stringArg, const char checkOnArg);
+bool endswithignorecase(const char *const stringArg, const char checkOnArg);
