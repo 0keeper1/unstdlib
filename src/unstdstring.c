bb08f1e5916f0c393405e71640ebf02507b074bb
HeavySin <lifeisbug0@gmail.com>
2023-09-14 03:26:43 +0300

    function `tolowerstrcopy` implemented & added.

--- src/unstdstring.c
+++ src/unstdstring.c
@@ -1,12 +1,36 @@
 #include "unstdstring.h"
+#include <stddef.h>
+#include <string.h>
 
-bool endswith(const char *const stringArg, const char checkOnArg) {
-    if (stringArg[strlen(stringArg) - 1] == checkOnArg) { return true;}
-    else {return false;}
+char *tolowerstrcopy(const char *strArg) {
+  size_t strLength = strlen(strArg);
+  char *allocationPtr = (char *)calloc(strLength + 1, sizeof(char));
+
+  if (allocationPtr == NULL) {
+    fprintf(stderr, "%.25s", "Failed to allocate memory");
+    return NULL;
+  }
+
+  for (int i = 0; i < strLength; i++) {
+    allocationPtr[i] = tolower((unsigned char)strArg[i]);
+  }
+
+  allocationPtr[strLength] = '\0';
+  return allocationPtr;
 }
 
+bool endswith(const char *const stringArg, const char checkOnArg) {
+  if (stringArg[strlen(stringArg) - 1] == checkOnArg) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 bool endswithignorecase(const char *const stringArg, const char checkOnArg) {
-    if (tolower(stringArg[strlen(stringArg) - 1]) == tolower(checkOnArg)) { return true;}
-    else {return false;}
+  if (tolower(stringArg[strlen(stringArg) - 1]) == tolower(checkOnArg)) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- src/unstdstring.h
+++ src/unstdstring.h
@@ -1,6 +1,9 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+char *tolowerstrcopy(const char *strArg);
 bool endswith(const char *const stringArg, const char checkOnArg);
 bool endswithignorecase(const char *const stringArg, const char checkOnArg);
