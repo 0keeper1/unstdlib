48b1824c3db397a07aab7f899e712512a554023d
HeavySin <lifeisbug0@gmail.com>
2023-09-14 04:25:49 +0300

    functions `tolower/upperstr` & `tolower/upperstrcopy` implemented & added.

--- src/unstdstring.c
+++ src/unstdstring.c
@@ -1,5 +1,10 @@
 #include "unstdstring.h"
 
+char *tolowerstr(char (*strArg)[]) {
+  for (size_t i = 0; (*strArg)[i]; i++) {(*strArg)[i] = tolower((*strArg)[i]);}
+  return *strArg;
+}
+
 char *tolowerstrcopy(const char *const strArg) {
   size_t strLength = strlen(strArg);
   char *allocationPtr = (char *)calloc(strLength + 1, sizeof(char));
@@ -11,6 +16,29 @@ char *tolowerstrcopy(const char *const strArg) {
 
   for (int i = 0; i < strLength; i++) {
     allocationPtr[i] = tolower((unsigned char)strArg[i]);
+  }
+
+  allocationPtr[strLength] = '\0';
+  return allocationPtr;
+}
+char *toupperstr(char (*strArg)[]) {
+  for (size_t i = 0; (*strArg)[i]; i++) {
+    (*strArg)[i] = toupper((*strArg)[i]);
+  }
+  return *strArg;
+}
+
+char *toupperstrcopy(const char *const strArg) {
+  size_t strLength = strlen(strArg);
+  char *allocationPtr = (char *)calloc(strLength + 1, sizeof(char));
+
+  if (allocationPtr == NULL) {
+    fprintf(stderr, "%.25s", "Failed to allocate memory");
+    return NULL;
+  }
+
+  for (int i = 0; i < strLength; i++) {
+    allocationPtr[i] = toupper((unsigned char)strArg[i]);
   }
 
   allocationPtr[strLength] = '\0';
--- src/unstdstring.h
+++ src/unstdstring.h
@@ -1,9 +1,13 @@
 #include <ctype.h>
 #include <stdbool.h>
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+char *tolowerstr(char (*strArg)[]);
 char *tolowerstrcopy(const char *const strArg);
+char *toupperstr(char (*strArg)[]);
+char *toupperstrcopy(const char *const strArg);
+
 bool endswith(const char *const stringArg, const char checkOnArg);
 bool endswithignorecase(const char *const stringArg, const char checkOnArg);
