7cc199b7072efa7322aec302f7a4a072c11092b6
HeavySin <lifeisbug0@gmail.com>
2023-11-01 13:44:21 +0300

    code documentation insights migrated to header files.

--- src/unstdstring.c
+++ src/unstdstring.c
@@ -1,50 +1,26 @@
 #include "unstdstring.h"
 
 
-/**
- *
- * @param fBufferArg a pointer to a null-terminated buffer
- * @param sBufferArg a pointer to a null-terminated buffer
- * @return true if both buffers are identical to each other otherwise false
- */
 bool unstd_strcmp(const char *const fBufferArg, const char *const sBufferArg) {
     return fBufferArg != NULL && sBufferArg != NULL ? !strcmp(fBufferArg, sBufferArg) : false;
 }
 
-/**
- *
- * @param bufferArg a pointer to a null-terminated buffer
- * @param checkOnArg the character which the function performs the operation on
- * @return a boolean indicating the state of operation
- */
+
 bool unstd_endswithchar(const char *const bufferArg, const char checkOnArg) {
     return bufferArg[strlen(bufferArg) - 1] == checkOnArg;
 }
 
-/**
- *
- * @param bufferArg a pointer to a null-terminated buffer
- * @param checkOnArg the character which the function performs the operation on
- * @return a boolean indicating the state of operation
- */
+
 bool unstd_endswithcharignorecase(const char *const bufferArg, const char checkOnArg) {
     return tolower(bufferArg[strlen(bufferArg) - 1]) == tolower(checkOnArg);
 }
 
-/**
- *
- * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array buffer
- * @return a pointer to the first character of the modified buffer
- */
+
 void unstd_tolowerstr(char *const bufferArg) {
     for (size_t i = 0; bufferArg[i] != '\0'; bufferArg[i] = (char) tolower(bufferArg[i]), i++);
 }
 
-/**
- *
- * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array / constant buffer
- * @return a pointer to the first character of the modified buffer
- */
+
 char *unstd_tolowerstrcopy(const char *const bufferArg) {
     char *allocationPtr = (char *) calloc(strlen(bufferArg) + 1, sizeof(char));
     if (allocationPtr == NULL) {
@@ -58,29 +34,17 @@ char *unstd_tolowerstrcopy(const char *const bufferArg) {
     return allocationPtr;
 }
 
-/**
- *
- * @param bufferArg should be a reference to a null-terminated c-array of characters
- * @return a pointer to the first character of the modified buffer
- */
+
 void unstd_tolowerstrarray(char (*const bufferArg)[]) {
     for (size_t i = 0; (*bufferArg)[i]; i++) { (*bufferArg)[i] = (char) tolower((*bufferArg)[i]); }
 }
 
-/**
- *
- * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array buffer
- * @return a pointer to the first character of the modified buffer
- */
+
 void unstd_toupperstr(char *const bufferArg) {
     for (size_t i = 0; bufferArg[i] != '\0'; bufferArg[i] = (char) toupper(bufferArg[i]), i++);
 }
 
-/**
- *
- * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array buffer
- * @return a pointer to the first character of the modified buffer
- */
+
 char *unstd_toupperstrcopy(const char *const bufferArg) {
     size_t strLength = strlen(bufferArg);
     char *allocationPtr = (char *) calloc(strLength + 1, sizeof(char));
@@ -95,66 +59,37 @@ char *unstd_toupperstrcopy(const char *const bufferArg) {
     return allocationPtr;
 }
 
-/**
- *
- * @param bufferArg should be a reference to a null-terminated c-array of characters
- * @return a pointer to the first character of the modified buffer
- */
+
 void unstd_toupperstrarray(char (*const bufferArg)[]) {
     for (size_t i = 0; (*bufferArg)[i]; i++) { (*bufferArg)[i] = (char) toupper((*bufferArg)[i]); }
 }
 
-/**
- *
- * @param bufferArg should be an ascii character
- * @return whether a character is an ASCII control character or not
- */
+
 bool unstd_isasciicontrolchar(const unsigned char bufferArg) {
     return bufferArg >= 0 && bufferArg <= 31 || bufferArg == 127;
 }
 
-/**
- *
- * @param bufferArg should be an ascii character
- * @return whether a character is an ASCII printable character or not
- */
+
 bool unstd_isasciiprintablechar(const unsigned char bufferArg) {
     return bufferArg >= 32 && bufferArg <= 126;
 }
 
-/**
- *
- * @param bufferArg should be an ascii character
- * @return whether a character is an ASCII extended character or not
- */
+
 bool unstd_isasciiextendedchar(const unsigned char bufferArg) {
     return bufferArg >= 128 && bufferArg <= 255;
 }
 
 
-/**
- *
- * @param bufferArg should be a character
- * @return whether a character is within the range A-Z, a-z or not
- */
 bool unstd_isalphabeticchar(const unsigned char bufferArg) {
     return bufferArg >= 'a' && bufferArg <= 'z' || bufferArg >= 'A' && bufferArg <= 'Z';
 }
 
-/**
- *
- * @param bufferArg should be a character
- * @return whether a character is within the range A-Z, a-z, 0-9 or not
- */
+
 bool unstd_isalphanumericchar(const unsigned char bufferArg) {
     return unstd_isalphabeticchar(bufferArg) || bufferArg >= '0' && bufferArg <= '9';
 }
 
-/**
- *
- * @param bufferArg should be a character
- * @return whether a character is within the range A-Z, a-z, 0-9 or not
- */
+
 bool unstd_isdigitchar(const unsigned char bufferArg) {
     return bufferArg >= '0' && bufferArg <= '9';
 }
--- src/unstdstring.h
+++ src/unstdstring.h
@@ -6,23 +6,119 @@
 #include <stdarg.h>
 
 
+//! [cmp]
+/**
+ *
+ * @param fBufferArg a pointer to a null-terminated buffer
+ * @param sBufferArg a pointer to a null-terminated buffer
+ * @return true if both buffers are identical to each other otherwise false
+ */
 bool unstd_strcmp(const char *const fBufferArg, const char *const sBufferArg);
 
+
+//! [endswith]
+/**
+ *
+ * @param bufferArg a pointer to a null-terminated buffer
+ * @param checkOnArg the character which the function performs the operation on
+ * @return a boolean indicating the state of operation
+ */
 bool unstd_endswithchar(const char *const bufferArg, const char checkOnArg);
+
+/**
+ *
+ * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array buffer
+ * @return a pointer to the first character of the modified buffer
+ */
 bool unstd_endswithcharignorecase(const char *const bufferArg, const char checkOnArg);
 
+
+//! [tolower]
+/**
+*
+* @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array buffer
+* @return a pointer to the first character of the modified buffer
+*/
 void unstd_tolowerstr(char *const bufferArg);
+
+/**
+ *
+ * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array / constant buffer
+ * @return a pointer to the first character of the modified buffer
+ */
 char *unstd_tolowerstrcopy(const char *const bufferArg);
+
+/**
+ *
+ * @param bufferArg should be a reference to a null-terminated c-array of characters
+ * @return a pointer to the first character of the modified buffer
+ */
 void unstd_tolowerstrarray(char (*const bufferArg)[]);
 
+
+//! [toupper]
+/**
+ *
+ * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array buffer
+ * @return a pointer to the first character of the modified buffer
+ */
 void unstd_toupperstr(char *const bufferArg);
+
+/**
+ *
+ * @param bufferArg should be a pointer to a null-terminated heap-allocated / c-array buffer
+ * @return a pointer to the first character of the modified buffer
+ */
 char *unstd_toupperstrcopy(const char *const bufferArg);
+
+/**
+ *
+ * @param bufferArg should be a reference to a null-terminated c-array of characters
+ * @return a pointer to the first character of the modified buffer
+ */
 void unstd_toupperstrarray(char (*const bufferArg)[]);
 
+
+//! [is]
+//! [isascii]
+/**
+*
+* @param bufferArg should be an ascii character
+* @return whether a character is an ASCII control character or not
+*/
 bool unstd_isasciicontrolchar(const unsigned char bufferArg);
+
+/**
+ *
+ * @param bufferArg should be an ascii character
+ * @return whether a character is an ASCII printable character or not
+ */
 bool unstd_isasciiprintablechar(const unsigned char bufferArg);
+
+/**
+ *
+ * @param bufferArg should be an ascii character
+ * @return whether a character is an ASCII extended character or not
+ */
 bool unstd_isasciiextendedchar(const unsigned char bufferArg);
 
+/**
+ *
+ * @param bufferArg should be a character
+ * @return whether a character is within the range A-Z, a-z or not
+ */
 bool unstd_isalphabeticchar(const unsigned char bufferArg);
+
+/**
+ *
+ * @param bufferArg should be a character
+ * @return whether a character is within the range A-Z, a-z, 0-9 or not
+ */
 bool unstd_isalphanumericchar(const unsigned char bufferArg);
+
+/**
+*
+* @param bufferArg should be a character
+* @return whether a character is within the range A-Z, a-z, 0-9 or not
+*/
 bool unstd_isdigitchar(const unsigned char bufferArg);
